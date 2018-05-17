a71fd76c6ca2db0b6ace1fb8e6ed8d97a829ec90
Salvador Marí <saruba@gmail.com>
2018-05-17 13:15:50 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var Tendarts = window.Tendarts || {};
+
+console.log(window.location.href);
+
+var url = new URL(window.location.href);
+var from = url.searchParams.get('from');
+console.log(from);
+if (from) {
+  //Only subscribe if param form is in the url
+
+  Tendarts.init({
+    token: 'ae78ae500e964ca9f3b4bd50dc48e42f6f9f77ea',
+    publicKey:
+      'BP9AhaIxlUspDadNAJTD6r3uinq6RbJh8NetzMwPg3zzbYYG4lX39jISja9BNyD1egzC_pou4m9KTKxj_m0Vgms',
+    autosubscribe: true,
+    geolocation: false,
+    debug: true,
+    key: {
+      label: 'publisher',
+      value: from,
+      kind: 2
+    }
+  });
+}
