a5ae95d7045ce71bb943117b0549e916d9aab055
Salvador Marí <saruba@gmail.com>
2018-05-18 15:55:32 +0200

    transitions

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ if (from) {
     scope: '/publisher-sample/',
     serviceWorkerPath: '/publisher-sample/'
   });
-  console.log('addEventListener 1');
+  console.log('addEventListener 2');
   document.addEventListener(
     'CREATE_DEVICE_EVENT',
     function() {
